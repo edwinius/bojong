b0aaf89940da3a3b96ac896190931cfaee67a78b
edwinius <edwinius@live.com>
2019-09-02 19:07:15 +0700

    Delete global.js

--- app/global.js
+++
@@ -1,9 +0,0 @@
-global.uri = 'http://bojonggenteng.sukabumikab.go.id/';
-global.api = 'http://13.250.133.237/';
-
-//global.uri = 'http://192.168.43.174/sukabumi/';
-//global.api = 'http://192.168.43.174/sukabumiapi/';
-
-global.s3 = 'https://s3-ap-southeast-1.amazonaws.com/bojonggenteng/'
-global.appToken = '$2y$10$.pslhtQN40mZurSqDm/Cku2Zq/4AqSZ8y3zwkeLRouM1LdmlLytJy';
-global.appVersion = 'V1.0.9(9)';
